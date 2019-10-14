e2a0d13f676f6d635d91ecd3fb4768effbc4288b
Leechuk <55555794+Leechuk@users.noreply.github.com>
2019-10-14 12:10:12 +0200

    Update 14102019

--- Assets/_GameObjects/Scripts/DisparadorSniper.cs
+++ Assets/_GameObjects/Scripts/DisparadorSniper.cs
@@ -7,6 +7,8 @@ public class DisparadorSniper : MonoBehaviour
     [SerializeField] GameObject bala; //Prefab
     [SerializeField] Transform pOrigen;
     [SerializeField] int fuerza = 100; //Fuerza que se implementa
+    [SerializeField] GameObject prefabImpacto;
+    
 
     void Start()
     {
@@ -21,16 +23,24 @@ public class DisparadorSniper : MonoBehaviour
             Disparo();
 
             print("Dispara");
+
+            Debug.DrawRay(pOrigen.position, pOrigen.forward * 100, Color.green);//color de seguimiento
         }
     }
 
     public void Disparo()
     {
         RaycastHit rch;
-        bool hayImpacto = Physics.Raycast(
+
+        Ray rayito = new Ray(pOrigen.position, pOrigen.forward);
+        bool hayImpacto = Physics.Raycast(rayito, out rch);
+        //Debug.DrawRay(pOrigen.position, pOrigen.forward * rch.distance, Color.red, Mathf.Infinity);
+        //Debug.DrawLine()
+
+        /*bool hayImpacto = Physics.Raycast(
             pOrigen.position,
             pOrigen.forward,
-            out rch);
+            out rch);*/
         if (hayImpacto)
         {
             print(rch.collider.gameObject.name);
@@ -38,8 +48,16 @@ public class DisparadorSniper : MonoBehaviour
             if (rch.collider.gameObject.GetComponent<Rigidbody>() !=null)
             {
                 print("Distancia" + rch.distance);// para saber la distancia del impacto
-                rch.collider.gameObject.GetComponent<Rigidbody>().
-                    AddForce(pOrigen.forward.normalized * fuerza);
+                //rch.collider.gameObject.GetComponent<Rigidbody>().
+                //    AddForce(pOrigen.forward.normalized * fuerza);
+                //GameObject impacto = Instantiate(prefabImpacto, rch.point, Quaternion.identity); //Impacto independiente
+                GameObject impacto = Instantiate(prefabImpacto, rch.transform);
+                //impacto.transform.SetParent();
+                impacto.transform.position = rch.point;
+                impacto.transform.rotation = Quaternion.FromToRotation(Vector3.back, rch.normal);
+                impacto.transform.Translate(Vector3.back * 0.01f);
+                Debug.DrawRay(rch.point, rch.normal, Color.blue, 10);
+
             }
         }
 
---
+++ Assets/_GameObjects/Scripts/Flecha.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flecha : MonoBehaviour
+{
+    Vector3 prevPos;
+    Vector3 currentPOS;
+    Vector3 difPos;
+    bool primeraVez = true;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<BoxCollider>().enabled = false;
+        transform.SetParent(other.transform);
+    }
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().isKinematic = true;
+        transform.SetParent(other.transform);
+    }*/
+
+    private void FixedUpdate()
+    {
+        currentPOS = transform.position;
+        if (!primeraVez)
+        {
+            difPos = currentPOS - prevPos;
+            this.transform.forward = difPos.normalized;
+        }
+        else
+        {
+            primeraVez = false;
+        }
+        prevPos = currentPOS;
+    }
+
+}
